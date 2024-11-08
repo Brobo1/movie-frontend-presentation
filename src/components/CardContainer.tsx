1fb9971811f6aa0f69030a05b3e53a407ca19475
brobo1 <armanahz95@gmail.com>
2024-11-08 13:28:28 +0100

    Add user favourites to CardContainer
    Extended the CardContainer component to fetch and display user favourites. Updated background color logic for movie cards based on whether they are in the user's favourites.

--- src/components/CardContainer.tsx
+++ src/components/CardContainer.tsx
@@ -2,21 +2,33 @@ import { Card } from "./Card.tsx";
 import { useEffect, useState } from "react";
 import { fetchMovies } from "../api/movies.tsx";
 import "./CardContainer.css";
+import { fetchFavouritesOfUser } from "../api/favourites.tsx";
 type Movie = {
   id: number;
   imgUrl: string;
   title: string;
 };
 
+type Favourite = {
+  id: number;
+  userId: number;
+  movieId: number;
+};
+
+type Favourites = Favourite[];
+
 type Movies = Movie[];
 
 export function CardContainer() {
   const [movies, setMovies] = useState<Movies>([]);
+  const [favourites, setFavourites] = useState<Favourites>([]);
 
   useEffect(() => {
     async function fetch() {
       const data = await fetchMovies();
+      const fav = await fetchFavouritesOfUser(1);
       setMovies(data);
+      setFavourites(fav);
     }
     fetch().catch((err) => console.log(err));
   }, []);
@@ -26,7 +38,9 @@ export function CardContainer() {
       <div id="card-container">
         {movies.map((movie) => (
           <Card
-            style={{ backgroundColor: "red" }}
+            style={{
+              backgroundColor: `${favourites.some((fav) => fav.movieId === movie.id) ? "red" : ""}`,
+            }}
             img={movie.imgUrl}
             title={movie.title}
             key={movie.id}
